eafbdaafa4a922164b2cf5bf865055b18dcdd3bc
ROHITH-1234 <rohith2004.9@gmail.com>
2025-08-04 16:28:49 +0530

    updated

--- portfolio/src/components/Content.jsx
+++ portfolio/src/components/Content.jsx
@@ -23,10 +23,10 @@ disableRotation={true}
 />
          <div className='flex flex-col md:flex-row gap-15'>
          <img src={MyPhoto} alt="Profile" className="rounded-lg shadow-lg max-w-4xl min-w-2xs px-7 py-13  z-10 max-h-3xl min-h-4xs " />
-         <div className="text-left p-20  ml-10 text-2xl px-6 py-12 backdrop-blur-sm">
+         <div className="text-left p-20  ml-10 text-2xl px-6 py-12 backdrop-blur-sm min-w-3xs max-w-full">
             <h1 className="text-5xl text-white"><b>Rohith U</b></h1>
             <h1 className="text-gray-300 p-4 rounded text-center italic "><b>Software Engineer</b></h1>
-            <p className="text-justify">
+            <p className="text-justify">  
                I am a dedicated Computer Science student eager to apply my skills in real-world projects while continuously expanding my
  knowledge. With hands-on experience in developing applications and websites, I have a strong foundation in programming
  concepts and a focus on delivering effective solutions. I am driven to contribute to organizational goals and further enhance my
